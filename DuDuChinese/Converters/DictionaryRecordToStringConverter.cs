7a9153b64cd43909079816d0ec6e61990f00d803
Bartek Pawlik <bartek.pawlik@outlook.com>
2016-08-14 22:42:02 +0800

    Converters for DictionaryRecord and LearningExercise to string added.
    Signed-off-by: Bartek Pawlik <bartek.pawlik@outlook.com>

---
+++ DuDuChinese/Converters/DictionaryRecordToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using CC_CEDICT.Universal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuDuChinese.Converters
+{
+    public class DictionaryRecordToStringConverter : Windows.UI.Xaml.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string culture)
+        {
+            if (value != null && value is DictionaryRecord)
+                return ((DictionaryRecord)value).Chinese.Simplified;
+            else
+                return "[]";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DuDuChinese/Converters/LearningExerciseToStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuDuChinese.Converters
+{
+    public class LearningExerciseToStringConverter : Windows.UI.Xaml.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string culture)
+        {
+            return Models.LearningEngine.GetDescription((Models.LearningExercise)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
