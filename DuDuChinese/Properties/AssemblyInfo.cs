b2347e3a72a73720b2ea7f00ef814e5f7064cea9
Bartek Pawlik <bartek.pawlik@outlook.com>
2017-10-02 00:09:55 +0300

    DuDuChinese renamed to Chinese-English-汉字 Dictionary
    Signed-off-by: Bartek Pawlik <bartek.pawlik@outlook.com>

--- DuDuChinese/Properties/AssemblyInfo.cs
+++ DuDuChinese/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DuDuChinese")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Bartek Pawlik 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
